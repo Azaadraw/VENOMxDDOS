4fabd83601dcb5cf93df3660be84c181a8347c65
Azaadraw <aajadraw6@gmail.com>
2024-08-31 19:13:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7255048090:AAFpMpH9KDNrr3BpnlAlraE5c2UkqhlSY5w")
+BOT_TOKEN=("6817648796:AAFDYa01ohKJ27xngc9KWEmtpQhjPdrF1TU")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("")
+OWNER_USERNAME=("Udaybit")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("")
+ADMIN_IDS=("7518876598")
 
 
 
